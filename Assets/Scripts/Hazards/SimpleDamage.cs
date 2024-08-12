cff1dcf81478f6227abc52fc09579319939bec65
Jackson Lucas <s210201@students.aie.edu.au>
2024-08-12 16:35:42 +0930

    Added Level to scene
    > also added basic hazard and damage
    
    > adjusted camera angle
    
    > hazards can now trigger functions when toggled

---
+++ Assets/Scripts/Hazards/SimpleDamage.cs
@@ -0,0 +1,38 @@
+using ILOVEYOU.EnemySystem;
+using ILOVEYOU.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ILOVEYOU
+{
+    namespace Hazards
+    {
+        public class SimpleDamage : MonoBehaviour
+        {
+            [SerializeField] private float m_damage;
+            [SerializeField] private float m_rate;
+            private float m_time;
+            public void DealDamage(GameObject target)
+            {
+                if (m_time <= 0)
+                {
+                    Debug.Log("Fuck");
+                    PlayerControls player = target.GetComponent<PlayerControls>();
+
+                    if (player)
+                        player.TakeDamage(m_damage);
+
+                    m_time = m_rate;
+                }
+            }
+            private void Update()
+            {
+                if(m_time > 0)
+                {
+                    m_time -= m_rate * Time.deltaTime;
+                }
+            }
+        }
+    }
+}
