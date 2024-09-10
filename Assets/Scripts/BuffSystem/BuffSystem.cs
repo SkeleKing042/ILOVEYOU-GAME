b022c7423b424486c20cf47ebfbd758b266d93d9
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-09-10 15:29:42 +0930

    More progress on buff system
    - Can create new buffs
    - can delete buffs
    - can hide or show buffs
    - can change type of buffs
    - can undo any changes made to script

--- Assets/Scripts/BuffSystem/BuffSystem.cs
+++ Assets/Scripts/BuffSystem/BuffSystem.cs
@@ -1,41 +1,88 @@
+using ILOVEYOU.ProjectileSystem;
 using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering;
 
-public class BuffSystem : MonoBehaviour
+namespace ILOVEYOU
 {
-    [Serializable]
-    public class BuffData
+    namespace BuffSystem
     {
-        [SerializeField] private int m_buffType = 0;
-        [SerializeField] private bool m_isPermanent = false;
-        [SerializeField] private float m_time = 1f;
-        [SerializeField] private UnityEvent m_OnActivate;
+        public class BuffSystem : MonoBehaviour
+        {
+            [Serializable]
+            public class BuffData
+            {
+                //Generic
+                [SerializeField] private string m_name = "";
+                [SerializeField] private int m_buffType = 0; //0 = stats change, 1 = bullet change, 2 = Unity Event
+                [SerializeField] private bool m_isPermanent = false;
+                [SerializeField] private float m_time = 1f;
+                [SerializeField] private GameObject m_particleEffect;
 
-        public int GetBuffType { get { return m_buffType; } }
-        public float GetTime { get {return m_time; } }
+                //Values for stat change
+                [SerializeField] private float m_maxHealthValue = 0f;
+                [SerializeField] private float m_moveSpeedValue = 0f;
+                [SerializeField] private float m_shootSpeedValue = 0f;
+                [SerializeField] private float m_damageValue = 0f;
+                
+                //values for bullet change
+                [SerializeField] private BulletPattern m_pattern;
+
+                //Events for Event Type
+                [SerializeField] private UnityEvent m_onActivate;
+                [SerializeField] private UnityEvent m_onExpire;
+
+                public string GetName { get { return m_name; } }
+                public int GetBuffType { get { return m_buffType; } }
+                public bool GetPermanent { get { return m_isPermanent; } }
+                public float GetTime { get { return m_time; } }
+
+                public float GetMaxHealth { get { return m_maxHealthValue; } }
+                public float GetMoveSpeed { get { return m_moveSpeedValue; } }
+                public float GetShootSpeed { get { return m_shootSpeedValue; } }
+                public float GetDamage { get { return m_damageValue; } }
 
 
-        public void SetBuffType(int type) => m_buffType = type;
-        public void SetTime(float time) => m_time = time;
-    }
 
-    [SerializeField] private BuffData[] m_buffData = new BuffData[2];
+                public void SetName(string name) => m_name = name;
+                public void SetBuffType(int type) => m_buffType = type;
+                public void SetPermanent(bool set) => m_isPermanent = set;
+                public void SetTime(float time) => m_time = time;
 
-    public BuffData[] GetData { get { return m_buffData; } }
+                public void SetMaxHealth(float health) => m_maxHealthValue = health;
+                public void SetMoveSpeed(float speed) => m_moveSpeedValue = speed;
+                public void SetShootSpeed(float speed) => m_shootSpeedValue = speed;
+                public void SetDamage(float damage) => m_damageValue = damage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            [SerializeField] private BuffData[] m_buffData = new BuffData[0];
+
+            public BuffData[] GetData { get { return m_buffData; } }
+
+
+
+
+            #region DataEditing
+#if UNITY_EDITOR
+            public void ChangeData(BuffData[] data)
+            {
+                m_buffData = data;
+            }
+
+            public void CreateData()
+            {
+                Array.Resize(ref m_buffData, m_buffData.Length + 1);
+            }
+#endif
+            #endregion
+        }
+
     }
 }
+
--- Assets/Scripts/BuffSystem/Editor/BuffSystemEditor.cs
+++ Assets/Scripts/BuffSystem/Editor/BuffSystemEditor.cs
@@ -1,48 +1,168 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
 
-[CustomEditor(typeof(BuffSystem))]
-public class BuffSystemEditor : Editor
+namespace ILOVEYOU
 {
-    private readonly string[] m_effectNames = new string[] { "Stat Increase", "Unity Event", "Blah blah blah" };
-    private BuffSystem m_system;
-
-
-    public override void OnInspectorGUI()
+    namespace BuffSystem
     {
-
-
-        for (int i = 0; i < m_system.GetData.Length; i++)
+        [CustomEditor(typeof(BuffSystem))]
+        public class BuffSystemEditor : Editor
         {
-            EditorGUILayout.BeginVertical();
-            EditorGUILayout.LabelField("Status Effect " + i + ":");
-            
-            EditorGUI.BeginChangeCheck();
+            private readonly string[] m_effectNames = new string[] { "Stat Change", "Bullet Change", "Unity Event", "Blah blah blah" };
+            private bool[] m_foldOuts;
+            private BuffSystem m_system;
 
-            BuffSystem.BuffData current = m_system.GetData[i];
 
-            int buffType = current.GetBuffType;
-
-            buffType = EditorGUILayout.Popup("Buff Type:", buffType, m_effectNames);
-
-            EditorGUILayout.EndVertical();
-
-            if (EditorGUI.EndChangeCheck())
+            public override void OnInspectorGUI()
             {
-                Undo.RecordObject(target, "Changed Status Effect " + i);
-                current.SetBuffType(buffType);
+                m_system = target as BuffSystem;
+                Array.Resize(ref m_foldOuts, m_system.GetData.Length);
+
+                for (int i = 0; i < m_system.GetData.Length; i++)
+                {
+                    EditorGUILayout.BeginVertical();
+                    EditorGUILayout.BeginHorizontal();
+
+                    //EditorGUILayout.LabelField("Status Effect " + i + ":");
+
+                    string title = (m_system.GetData[i].GetName == "") ? "Status Effect " + i + ":" : m_system.GetData[i].GetName + ":";
+
+                    m_foldOuts[i] = EditorGUILayout.Foldout(m_foldOuts[i], title);
+
+                    if (GUILayout.Button("Remove"))
+                    {
+                        _Delete(i);
+
+                        break;
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+
+                    if (m_foldOuts[i])
+                    {
+                        EditorGUILayout.Space();
+
+                        EditorGUI.BeginChangeCheck();
+
+                        BuffSystem.BuffData current = m_system.GetData[i];
+
+                        string name = current.GetName;
+                        int buffType = current.GetBuffType;
+                        bool isPermanent = current.GetPermanent;
+                        float time = current.GetTime;
+                        SerializedProperty particleEffect = serializedObject.FindProperty("m_buffData").GetArrayElementAtIndex(i).FindPropertyRelative("m_particleEffect"); ;
+
+                        float maxHealthValue = current.GetMaxHealth;
+                        float moveSpeedValue = current.GetMoveSpeed;
+                        float shootSpeedValue = current.GetShootSpeed;
+                        float damageValue = current.GetDamage;
+
+                        name = EditorGUILayout.TextField("Status Name: ", name);
+                        buffType = EditorGUILayout.Popup("Buff Type:", buffType, m_effectNames);
+                        isPermanent = EditorGUILayout.Toggle("Is Permanent:", isPermanent);
+                        if (!isPermanent) time = EditorGUILayout.FloatField("Buff Time:", time);
+                        EditorGUILayout.PropertyField(particleEffect, new GUIContent("Particle Effect: "));
+
+                        
+
+                        EditorGUILayout.Space();
+
+                        switch (buffType)
+                        {
+                            case 0://Stat Change
+                                maxHealthValue = EditorGUILayout.FloatField("Max Health Add: ", maxHealthValue);
+                                moveSpeedValue = EditorGUILayout.FloatField("Move speed Add: ", moveSpeedValue);
+                                shootSpeedValue = EditorGUILayout.FloatField("Shoot Speed Multiplier Add: ", shootSpeedValue);
+                                damageValue = EditorGUILayout.FloatField("Damage Multiplier Add: ", damageValue);
+                                break;
+                            case 1://Bullet change
+                                SerializedProperty bulletObject = serializedObject.FindProperty("m_buffData").GetArrayElementAtIndex(i).FindPropertyRelative("m_pattern");
+
+                                EditorGUILayout.PropertyField(bulletObject, new GUIContent("New Bullet Pattern: "));
+                                break;
+
+                            case 2://unity event
+
+                                SerializedProperty onActivate = serializedObject.FindProperty("m_buffData").GetArrayElementAtIndex(i).FindPropertyRelative("m_onActivate");
+                                SerializedProperty onExpire = serializedObject.FindProperty("m_buffData").GetArrayElementAtIndex(i).FindPropertyRelative("m_onExpire");
+
+                                EditorGUILayout.PropertyField(onActivate);
+                                if (!isPermanent) EditorGUILayout.PropertyField(onExpire);
+                                break;
+                            default:
+                                Debug.LogError("Idk how but you borked the Buff System");
+                                break;
+                        }
+
+                        
+
+                        if (EditorGUI.EndChangeCheck())
+                        {
+                            Undo.RecordObject(target, "Changed Status Effect " + i);
+
+                            current.SetName(name);
+                            current.SetBuffType(buffType);
+                            current.SetPermanent(isPermanent);
+                            if (!isPermanent) current.SetTime(time);
+
+                            if(current.GetBuffType == 0)
+                            {
+                                current.SetMaxHealth(maxHealthValue);
+                                current.SetMoveSpeed(moveSpeedValue);
+                                current.SetShootSpeed(shootSpeedValue);
+                                current.SetDamage(damageValue);
+                            }
+
+                            serializedObject.ApplyModifiedProperties();
+                        }
+                    }
+
+                    EditorGUILayout.EndVertical();
+                    EditorGUILayout.Space();
+                }
+
+
+                if (GUILayout.Button("Create new buff"))
+                {
+                    Undo.RecordObject(target, "Added new Status Effect");
+                    m_system.CreateData();
+                }
+
+            }
+
+
+            private void _Delete(int dataToRemove)
+            {
+
+                BuffSystem.BuffData[] newData = new BuffSystem.BuffData[m_system.GetData.Length - 1];
+                int count = 0;
+
+                for (int i = 0; i < m_system.GetData.Length; i++)
+                {
+                    if (dataToRemove == i) continue;
+
+                    newData[count] = m_system.GetData[i];
+                    count++;
+                }
+
+                Undo.RecordObject(target, "Removed Status Effect " + dataToRemove);
+                m_system.ChangeData(newData);
+
+            }
+
+            private void OnEnable()
+            {
+
             }
         }
 
-
-        
-
-    }
-
-    private void OnEnable()
-    {
-        m_system = target as BuffSystem;
     }
 }
+
