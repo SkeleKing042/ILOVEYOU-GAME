b2e0255461b89c15b0c12252e8116d29a192d132
Jackson Lucas <s210201@students.aie.edu.au>
2024-09-16 12:15:19 +0930

    Update DamageBlink.cs

--- Assets/Shaders/DamageBlink/DamageBlink.cs
+++ Assets/Shaders/DamageBlink/DamageBlink.cs
@@ -2,57 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageBlink : MonoBehaviour
+namespace ILOVEYOU
 {
-    [SerializeField] private Material m_blinkMat;
-    [SerializeField] private float m_activeTime;
-
-    List<MeshRenderer> m_renderers = new List<MeshRenderer>();
-
-    private void Awake()
+    namespace Shader
     {
-        MeshRenderer ren = GetComponent<MeshRenderer>();
-        if (ren)
-            m_renderers.Add(ren);
-
-        MeshRenderer[] rens = GetComponentsInChildren<MeshRenderer>();
-        if(rens.Length > 0)
-            m_renderers.AddRange(rens);
-    }
-    public void StartBlink()
-    {
-        foreach (MeshRenderer renderer in m_renderers)
+        public class DamageBlink : MonoBehaviour
         {
-            List<Material> mats = new();
-            for (int i = 0; i < renderer.materials.Length; i++)
+            [SerializeField] private Material m_blinkMat;
+            [SerializeField] private float m_activeTime;
+
+            List<MeshRenderer> m_renderers = new List<MeshRenderer>();
+
+            private void Awake()
             {
-                mats.Add(renderer.materials[i]);
+                MeshRenderer ren = GetComponent<MeshRenderer>();
+                if (ren)
+                    m_renderers.Add(ren);
+
+                MeshRenderer[] rens = GetComponentsInChildren<MeshRenderer>();
+                if (rens.Length > 0)
+                    m_renderers.AddRange(rens);
             }
-            mats.Add(m_blinkMat);
-
-            renderer.materials = new Material[mats.Count];
-            renderer.materials = mats.ToArray();
-
-        }
-        Invoke("EndBlink", m_activeTime);
-    }
-    public void EndBlink()
-    {
-        foreach (MeshRenderer renderer in m_renderers)
-        {
-            List<Material> mats = new();
-            mats.AddRange(renderer.materials);
-
-            for(int i = 0; i < renderer.materials.Length; i++)
+            public void StartBlink()
             {
-                if(mats[i].shader == m_blinkMat.shader)
+                foreach (MeshRenderer renderer in m_renderers)
                 {
-                    mats.RemoveAt(i);
-                    break;
+                    List<Material> mats = new();
+                    for (int i = 0; i < renderer.materials.Length; i++)
+                    {
+                        mats.Add(renderer.materials[i]);
+                    }
+                    mats.Add(m_blinkMat);
+
+                    renderer.materials = new Material[mats.Count];
+                    renderer.materials = mats.ToArray();
+
+                }
+                Invoke("EndBlink", m_activeTime);
+            }
+            public void EndBlink()
+            {
+                foreach (MeshRenderer renderer in m_renderers)
+                {
+                    List<Material> mats = new();
+                    mats.AddRange(renderer.materials);
+
+                    for (int i = 0; i < renderer.materials.Length; i++)
+                    {
+                        if (mats[i].shader == m_blinkMat.shader)
+                        {
+                            mats.RemoveAt(i);
+                            break;
+                        }
+                    }
+                    renderer.materials = new Material[mats.Count];
+                    renderer.materials = mats.ToArray();
                 }
             }
-            renderer.materials = new Material[mats.Count];
-            renderer.materials = mats.ToArray();
         }
     }
 }
