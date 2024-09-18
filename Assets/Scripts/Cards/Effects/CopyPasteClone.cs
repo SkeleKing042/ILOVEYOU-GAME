0c1c5a2a3cb2db82e9735ea9ae122172343c4e77
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-09-18 12:21:33 +0930

    Fully Implemented copy paste
    - Card prefab added
    - Can stack multiple unlimited times
    - Currently takes the current weapon of the player
    - Clones circle around the player, positions dynamically change based on amount of clones

--- Assets/Scripts/Cards/Effects/CopyPasteClone.cs
+++ Assets/Scripts/Cards/Effects/CopyPasteClone.cs
@@ -1,29 +1,49 @@
+using ILOVEYOU.ProjectileSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CopyPasteClone : MonoBehaviour
+namespace ILOVEYOU
 {
-
-    private Transform m_target;
-    private Rigidbody m_rb;
-
-    // Start is called before the first frame update
-    void Start()
+    namespace Cards
     {
-        m_rb = GetComponent<Rigidbody>();
-    }
+        public class CopyPasteClone : MonoBehaviour
+        {
 
-    private void Initialize(Transform target)
-    {
-        m_target = target;
-    }
+            private Transform m_target;
+            private Rigidbody m_rb;
+            private BulletPatternObject m_bulletPattern;
+            private BulletPattern m_pattern;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        m_rb.position = Vector3.MoveTowards(m_rb.position, m_target.position, 10f * Time.fixedDeltaTime);
+            // Start is called before the first frame update
+            void Start()
+            {
 
-        m_rb.velocity = Vector3.zero;
+
+            }
+
+            public void Initialize(Transform target, BulletPatternObject bulletPattern)
+            {
+                m_rb = GetComponent<Rigidbody>();
+                m_pattern = GetComponent<BulletPattern>();
+
+                m_target = target;
+                m_bulletPattern = bulletPattern;
+
+                m_pattern.ChangePattern(m_bulletPattern);
+            }
+
+            // Update is called once per frame
+            void FixedUpdate()
+            {
+                m_rb.position = Vector3.MoveTowards(m_rb.position, m_target.position, 11f * Time.fixedDeltaTime);
+
+                m_rb.velocity = Vector3.zero;
+
+                m_pattern.PatternUpdate();
+            }
+        }
+
     }
 }
+
