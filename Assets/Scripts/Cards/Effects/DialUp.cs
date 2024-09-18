72743c0d8850d49137a173f712bb2d08877016af
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-09-18 16:28:17 +0930

    Started DialUp and added custom buff runtime creation
    - Can create buffs during runtime (however it is only buffs that influence player stats)
    - Dial Up functions (inconsistently gives a speed debuff)

---
+++ Assets/Scripts/Cards/Effects/DialUp.cs
@@ -0,0 +1,32 @@
+using ILOVEYOU.BuffSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialUp : MonoBehaviour
+{
+    private float m_time = 0f;
+    private BuffDataSystem m_buffDataSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_buffDataSystem = GetComponentInParent<BuffDataSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_time -= Time.deltaTime;
+
+        if(m_time <= 0f)
+        {
+            float range = Random.Range(0.05f, 1f);
+
+            //do thing
+            m_buffDataSystem.GiveBuff(new BuffDataSystem.BuffData("Slowness", 6969, 0, false, range, 0f, -5, 0, 0));
+
+            m_time = Random.Range(0.1f,1f) + range;
+        }
+    }
+}
