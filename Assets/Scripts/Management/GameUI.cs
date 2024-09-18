b4ac4423667f892e047957b2791221fbf5699bb1
Jackson Lucas <s210201@students.aie.edu.au>
2024-09-18 15:38:05 +0930

    Split GM UI into separate script.

---
+++ Assets/Scripts/Management/GameUI.cs
@@ -0,0 +1,39 @@
+using ILOVEYOU.Management;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace ILOVEYOU
+{
+    namespace UI
+    {
+        public class GameUI : MonoBehaviour
+        {
+            [SerializeField] private Color m_importantColour;
+
+            [Header("In-Game Menu")]
+            [SerializeField] private GameObject m_InGameSharedUI;
+            [SerializeField] private TextMeshProUGUI m_timerText;
+
+            [Header("Victory Menu")]
+            [SerializeField] private GameObject m_winScreen;
+            [SerializeField] private TextMeshProUGUI m_winText;
+            [SerializeField] private Button m_restartButton;
+            // Start is called before the first frame update
+            public void DisplayWinScreen(int winnerID)
+            {
+                m_winScreen.SetActive(true);
+                m_winText.text = $"Player {winnerID} wins!\nScore: {GameManager.GetScore.x} - {GameManager.GetScore.y}";
+                EventSystem.current.SetSelectedGameObject(m_restartButton.gameObject);
+            }
+            public void UpdateTimer(float currentTime)
+            {
+                Color timeColour = Color.white - new Color(1-m_importantColour.r, 1-m_importantColour.g, 1-m_importantColour.b) * Mathf.Clamp(GameManager.Instance.PercentToMaxDiff, 0, 1);
+                m_timerText.text = $"<color=#{ColorUtility.ToHtmlStringRGB(timeColour)}>{(int)currentTime}</color>";
+            }
+        }
+    }
+}
