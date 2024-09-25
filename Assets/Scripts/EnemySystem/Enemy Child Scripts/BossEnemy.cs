76a2e1d7bbc865e6ca0f5a514aa2cf64a9d9bdb6
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-09-25 13:47:15 +0930

    Boss complete
    - Hid some gizmos behind selection
    - redid text on cards to make more readable
    - boss fully implemented

--- Assets/Scripts/EnemySystem/Enemy Child Scripts/BossEnemy.cs
+++ Assets/Scripts/EnemySystem/Enemy Child Scripts/BossEnemy.cs
@@ -1,3 +1,5 @@
+using ILOVEYOU.Management;
+using ILOVEYOU.Player;
 using ILOVEYOU.ProjectileSystem;
 using JetBrains.Annotations;
 using System.Collections;
@@ -10,18 +12,37 @@ namespace ILOVEYOU
     {
         public class BossEnemy : Enemy
         {
+            public static BossEnemy[] Instances = new BossEnemy[2];
+
             BulletPattern m_pattern;
             [SerializeField] private float m_lungeTime = .3f;
             [SerializeField] private float m_lungeSpeed = 20f;
+            [SerializeField] private BulletPatternObject[] m_bossPatterns;
 
             private float m_lungeCooldown;
             private float m_tempSpeed = 0f;
+            private float m_maxSpeed = 0f;
+
+            private bool m_charging = false;
 
             public override void Initialize(Transform target)
             {
+                
                 m_lungeCooldown = m_lungeTime;
                 base.Initialize(target);
                 m_pattern.AddTarget(m_playerTransform);
+
+                m_maxSpeed = m_speed;
+
+                if(Instances[m_playerTransform.GetComponent<PlayerManager>().GetPlayerID])
+                {
+                    Instances[m_playerTransform.GetComponent<PlayerManager>().GetPlayerID].m_health += 200;
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    Instances[m_playerTransform.GetComponent<PlayerManager>().GetPlayerID] = this;
+                }
             }
 
 
@@ -40,6 +61,7 @@ namespace ILOVEYOU
                 }
                 else
                 {
+                    m_speed = m_maxSpeed;
                     MoveToTarget();
                 }
                 
@@ -47,12 +69,13 @@ namespace ILOVEYOU
 
             public override void DoNearAction()
             {
-                if (m_tempSpeed <= 0)
+                if (m_tempSpeed >= 0)
                 {
                     m_rigidBody.MovePosition(m_rigidBody.position + (m_tempSpeed * Time.deltaTime * transform.forward));
                     m_tempSpeed -= Time.deltaTime * (m_lungeSpeed / 2);
+                    m_rigidBody.velocity = Vector3.zero;
                 }
-                else if (Vector3.Distance(transform.position, m_playerTransform.position) < 5f)
+                else if (m_charging)
                 {
                     //gets relative position between the player and enemy
                     Vector3 relativePos = m_playerTransform.position - transform.position;
@@ -67,15 +90,47 @@ namespace ILOVEYOU
                     {
                         m_tempSpeed = m_lungeSpeed;
                         m_lungeCooldown = m_lungeTime;
+                        //disengages charging sequence
+                        m_pattern.ChangePattern(m_bossPatterns[1]);
+                        m_charging = false;
                     }
+
+                    m_pattern.PatternUpdate();
+                }
+                //lunging
+                else if (Vector3.Distance(transform.position, m_playerTransform.position) < m_distanceCondition / 2f && !m_charging)
+                {
+                    //engages charging sequence
+                    m_pattern.ChangePattern(m_bossPatterns[0]);
+                    m_charging = true;
                 }
                 else
                 {
+                    //gets relative position between the player and enemy
+                    Vector3 relativePos = m_playerTransform.position - transform.position;
+                    //looks at the player (removing x, and z rotation)
+                    Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+                    rotation = Quaternion.Euler(0f, rotation.eulerAngles.y, 0f);
+                    transform.rotation = rotation;
+
+                    //shooting
                     m_pattern.PatternUpdate();
+                    m_speed = m_maxSpeed / 2f;
+                    MoveToTarget();
                 }
+                
+            }
+
+            private void OnDrawGizmos()
+            {
+                Gizmos.DrawWireSphere(transform.position, m_distanceCondition);
+                //Gizmos.DrawWireSphere(transform.position, 24 / 1.5f);
+                Gizmos.DrawWireSphere(transform.position, m_distanceCondition / 2f);
             }
         }
     }
+
+    
 }
 
 
