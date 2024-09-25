be10fb4f297ba883b878d15acaaf3440a8fea480
Jackson Lucas <s210201@students.aie.edu.au>
2024-09-25 11:45:30 +0930

    Made player shoot audio shift pitch randomly.
    Also made BGM loop.

---
+++ Assets/Scripts/Audio/RadoPitchShift.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RadoPitchShift : MonoBehaviour
+{
+    [SerializeField] private Vector2 m_minMax;
+    private AudioSource m_source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_source = GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+        float rnd = Random.Range(m_minMax.x, m_minMax.y);
+        m_source.pitch = rnd;
+        m_source.Play();
+    }
+}
