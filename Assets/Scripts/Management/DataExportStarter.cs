9c1547ec56936de531948acbf1def914e5c65e12
Jackson <s210201@students.aie.edu.au>
2024-09-27 16:11:46 +0930

    Added intro scene for analysis mode
    made a build too

--- Assets/Scripts/Management/DataExportStarter.cs
+++ Assets/Scripts/Management/DataExportStarter.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DataExporter
 {
     public class DataExportStarter : MonoBehaviour
     {
+        [SerializeField] private string m_targetScene;
         // Start is called before the first frame update
         void Start()
         {
             DataExport.Init();
+        }
+        public void ChangeScene()
+        {
+            SceneManager.LoadSceneAsync(m_targetScene);
         }
     }
 }
