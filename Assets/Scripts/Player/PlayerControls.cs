a3c491e89eeea15fc28f2d88c0b497ced9910bb3
Jackson Lucas <s210201@students.aie.edu.au>
2024-10-15 14:30:30 +1030

    Revert "Revert "Merge branch 'main' into Jackson-PlayerUIUpdate""
    This reverts commit 3a0c11ecd92b49c1b7c4683472f185e9e5f9d7cd.

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -25,6 +25,7 @@ namespace ILOVEYOU
             [Header("General")]
             [SerializeField] private float m_MaxHealth = 10f;
             private float m_health;
+            public float GetHealthPercent => m_health / m_MaxHealth;
             [SerializeField] private float m_iframesTotal = 1f; //this is in seconds
             private float m_iframesCurrent;
             [SerializeField] private Animator m_anim; //animator should be located on the player model
@@ -187,10 +188,10 @@ namespace ILOVEYOU
                 float moveAngle = Mathf.Rad2Deg * Mathf.Atan2(m_moveDir.x, m_moveDir.z);
                 //gets quaternions to convert to vectors
                 Quaternion moveQ = Quaternion.Euler(0f, moveAngle, 0f);
-                Quaternion shütQ = m_facingObject.rotation;
+                Quaternion shotQ = m_facingObject.rotation;
                 //sets required animation variables
-                m_anim.SetFloat("moveX", (moveQ * Quaternion.Inverse(shütQ) * Vector3.forward * m_moveDir.magnitude).x);
-                m_anim.SetFloat("moveZ", (moveQ * Quaternion.Inverse(shütQ) * Vector3.forward * m_moveDir.magnitude).z);
+                m_anim.SetFloat("moveX", (moveQ * Quaternion.Inverse(shotQ) * Vector3.forward * m_moveDir.magnitude).x);
+                m_anim.SetFloat("moveZ", (moveQ * Quaternion.Inverse(shotQ) * Vector3.forward * m_moveDir.magnitude).z);
             }
             public void OnFire(InputValue value)
             {
