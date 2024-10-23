dcd37a39e3c6f4c9852af9a22113e6ff2471c8fb
Jackson Lucas <s210201@students.aie.edu.au>
2024-10-23 15:23:52 +1030

    Made editor for color prefs

---
+++ Assets/Scripts/Management/ColorPrefEditor.cs
@@ -0,0 +1,83 @@
+using BgTools.PlayerPrefsEditor;
+using ILOVEYOU.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ILOVEYOU.EditorScript
+{
+    public class ColorPrefEditor : EditorWindow
+    {
+        public List<string> keys = new();
+        VisualElement m_root;
+        List<ColorField> m_colorFields = new();
+        [MenuItem("Tools/ColorPrefs Editor", false, 1)]
+        static void ShowWindow()
+        {
+            ColorPrefEditor wnd = GetWindow<ColorPrefEditor>();
+            wnd.titleContent = new GUIContent("Color Prefs Editor");
+        }
+        public void CreateGUI()
+        {
+            //initialize
+            m_root = rootVisualElement;
+
+            m_root.Clear();
+
+            m_colorFields = new();
+            keys = new();
+
+            keys.Add("Important Color");
+            keys.Add("Hazard color R");
+            keys.Add("Buff color R");
+            keys.Add("Debuff color R");
+            keys.Add("Summon color R");
+
+            foreach(var color in keys)
+            {
+                Label name = new(color);
+                m_root.Add(name);
+                ColorField cf = new();
+                cf.value = ColorPref.Get(color);
+                m_colorFields.Add(cf);
+                m_root.Add(cf);
+            }
+            //Func<VisualElement> makeItem = () => new Label();
+            //Action<VisualElement, int> bindItem = (e, i) => (e as Label).text = keys[i];
+            ///const int height = 16;
+
+            //ListView field = new ListView(keys, height, makeItem, bindItem);
+/*            List<TextField> keylist = new();
+            foreach (var key in keys)
+            {
+                TextField t = new TextField();
+                t.label = key;
+                keylist.Add(t);
+            }*/
+            //m_root.Add(field);
+
+           /* {
+                ColorField cf = new ColorField();
+                cf.value = ColorPref.Get($"{key}");
+                m_root.Add(cf);
+
+            }*/
+
+        }
+        private void OnGUI()
+        {
+        }
+        private void Update()
+        {
+            for(int i = 0; i < keys.Count; i++)
+            {
+                ColorPref.Set(keys[i], m_colorFields[i].value);
+            }
+            //ColorPref.Set("Important Color", m_colorField.value);
+        }
+    }
+}
