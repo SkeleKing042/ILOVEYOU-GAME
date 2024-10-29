fe66d37fbf7ba39531a84c540bb83fa086905c77
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-10-29 11:20:02 +1030

    Menu popups functional
    - menu popups now work
    - When closed the selected object will return to the last object that called them

--- Assets/Scripts/Management/MenuPopUp.cs
+++ Assets/Scripts/Management/MenuPopUp.cs
@@ -7,16 +7,28 @@ namespace ILOVEYOU.Management
 {
     public class MenuPopUp : MonoBehaviour
     {
+        [SerializeField] private GameObject m_firstSelected;
+        private GameObject m_returnObject;
+
         // Start is called before the first frame update
         void Start()
         {
-            
+            FindObjectOfType<EventSystem>().SetSelectedGameObject(m_firstSelected);
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Return()
         {
+            if (m_returnObject) FindObjectOfType<EventSystem>().SetSelectedGameObject(m_returnObject);
 
+            Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// sets return object for when the popup is closed
+        /// </summary>
+        public void SetReturn(GameObject returnObj)
+        {
+            m_returnObject = returnObj;
         }
     }
 }
