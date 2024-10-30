5be8f7f97bee224ef709a69172325eba5fd231b1
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-10-30 11:38:50 +1030

    Added footstep sounds
    - Footstep sound functionality on player
    TODO: adjust volume based on speed

---
+++ Assets/Scripts/Player/PlayerAnimatorEvents.cs
@@ -0,0 +1,44 @@
+using ILOVEYOU.Audio;
+using UnityEngine;
+
+namespace ILOVEYOU.Player
+{
+    public class PlayerAnimatorEvents : MonoBehaviour
+    {
+        private Animator m_animator;
+
+        private void Start()
+        {
+            m_animator = GetComponent<Animator>();
+        }
+
+        public void FootStep(AnimationEvent evt)
+        {
+            if (_IsHeaviestAnimClip(evt.animatorClipInfo.clip))
+            {
+                GetComponent<SoundManager>().PlayRandomSound(0);
+            }
+        }
+
+        private bool _IsHeaviestAnimClip(AnimationClip currentClip)
+        {
+            var currentAnimatorClipInfo = m_animator.GetCurrentAnimatorClipInfo(0);
+            float highestWeight = 0f;
+            AnimationClip highestWeightClip = null;
+
+            // Find the clip with the highest weight
+            foreach (var clipInfo in currentAnimatorClipInfo)
+            {
+                if (clipInfo.weight > highestWeight)
+                {
+                    highestWeight = clipInfo.weight;
+                    highestWeightClip = clipInfo.clip;
+                }
+            }
+
+            return highestWeightClip != null && currentClip == highestWeightClip;
+        }
+    }
+
+}
+
