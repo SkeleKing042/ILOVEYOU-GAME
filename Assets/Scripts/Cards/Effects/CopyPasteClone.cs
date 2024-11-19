036a6e04f2375fea2ca3e702d467051e640c7dea
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-11-19 16:58:08 +1030

    Started with clone

--- Assets/Scripts/Cards/Effects/CopyPasteClone.cs
+++ Assets/Scripts/Cards/Effects/CopyPasteClone.cs
@@ -7,6 +7,8 @@ namespace ILOVEYOU
 {
     namespace Cards
     {
+        [RequireComponent(typeof(BulletPattern))]
+        [RequireComponent(typeof(Rigidbody))]
         public class CopyPasteClone : MonoBehaviour
         {
 
@@ -14,19 +16,19 @@ namespace ILOVEYOU
             private Rigidbody m_rb;
             private BulletPatternObject m_bulletPattern;
             private BulletPattern m_pattern;
+            private Animator m_animator;
+            private Animator m_playerAnimator; //animator on the player
 
-            // Start is called before the first frame update
-            void Start()
+            private Vector3 m_lastLocation;
+
+            public void Initialize(Transform target, BulletPatternObject bulletPattern, Animator playerAnimator)
             {
-
-
-            }
-
-            public void Initialize(Transform target, BulletPatternObject bulletPattern)
-            {
+                //get components
                 m_rb = GetComponent<Rigidbody>();
                 m_pattern = GetComponent<BulletPattern>();
-
+                m_animator = GetComponentInChildren<Animator>();
+                m_playerAnimator = playerAnimator;
+                //point the clone to where it needs to go and give it a bullet pattern
                 m_target = target;
                 m_bulletPattern = bulletPattern;
 
@@ -34,13 +36,21 @@ namespace ILOVEYOU
             }
 
             // Update is called once per frame
-            void FixedUpdate()
+            void Update()
             {
-                m_rb.position = Vector3.MoveTowards(m_rb.position, m_target.position, 11f * Time.fixedDeltaTime);
+
+                Vector3 velocity = (transform.position - m_lastLocation);
+                Debug.Log(velocity.normalized);
+
+                m_animator.SetBool("Shooting", m_playerAnimator.GetBool("Shooting"));
+
+                m_rb.position = Vector3.MoveTowards(m_rb.position, m_target.position, 11f * Time.deltaTime);
 
                 m_rb.velocity = Vector3.zero;
 
-                m_pattern.PatternUpdate();
+                if (m_playerAnimator.GetBool("Shooting")) m_pattern.PatternUpdate();
+
+                m_lastLocation = transform.position;
             }
         }
 
