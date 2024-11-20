711caade4fc13ad64c0b4de5ca3ed4a0392d2204
Aidan-AIE <126209033+Aidan-AIE@users.noreply.github.com>
2024-11-20 10:42:06 +1030

    Clone update finished
    - clone now animates

--- Assets/Scripts/Cards/Effects/CopyPasteClone.cs
+++ Assets/Scripts/Cards/Effects/CopyPasteClone.cs
@@ -12,6 +12,8 @@ namespace ILOVEYOU
         public class CopyPasteClone : MonoBehaviour
         {
 
+            private float m_moveSpeed = 11f;
+
             private Transform m_target;
             private Rigidbody m_rb;
             private BulletPatternObject m_bulletPattern;
@@ -38,17 +40,31 @@ namespace ILOVEYOU
             // Update is called once per frame
             void Update()
             {
-
-                Vector3 velocity = (transform.position - m_lastLocation);
-                Debug.Log(velocity.normalized);
-
                 m_animator.SetBool("Shooting", m_playerAnimator.GetBool("Shooting"));
 
-                m_rb.position = Vector3.MoveTowards(m_rb.position, m_target.position, 11f * Time.deltaTime);
+                if (m_playerAnimator.GetBool("Shooting")) m_pattern.PatternUpdate();
+            }
+
+            private void FixedUpdate()
+            {
+                Vector3 velocity = (transform.position - m_lastLocation);
+
+                Vector3 dir = velocity.normalized * (velocity.magnitude / Time.fixedDeltaTime / m_moveSpeed);
+
+                //this is just a copy from the UpdateAnimator function from PlayerControls
+
+                //Converts movement into angle
+                float moveAngle = Mathf.Rad2Deg * Mathf.Atan2(dir.x, dir.z);
+                //gets quaternions to convert to vectors
+                Quaternion moveQ = Quaternion.Euler(0f, moveAngle, 0f);
+                Quaternion shotQ = transform.rotation;
+                //sets required animation variables
+                m_animator.SetFloat("moveX", (moveQ * Quaternion.Inverse(shotQ) * Vector3.forward * dir.magnitude).x);
+                m_animator.SetFloat("moveZ", (moveQ * Quaternion.Inverse(shotQ) * Vector3.forward * dir.magnitude).z);
+
+                m_rb.position = Vector3.MoveTowards(m_rb.position, m_target.position, m_moveSpeed * Time.fixedDeltaTime);
 
                 m_rb.velocity = Vector3.zero;
-
-                if (m_playerAnimator.GetBool("Shooting")) m_pattern.PatternUpdate();
 
                 m_lastLocation = transform.position;
             }
