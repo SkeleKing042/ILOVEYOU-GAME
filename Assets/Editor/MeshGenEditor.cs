8e478be010a5bbd60338a11ec51204cfd745f548
Jackson Lucas <s210201@students.aie.edu.au>
2024-11-25 14:26:34 +1030

    Fixed issue with the VHACD system.
    Meshes are now added to a scene's asset pool - the sidecar folder.

--- Assets/Editor/MeshGenEditor.cs
+++ Assets/Editor/MeshGenEditor.cs
@@ -14,6 +14,7 @@ namespace ILOVEYOU.Environment
         }
         public override void OnInspectorGUI()
         {
+            EditorGUILayout.HelpBox("This script will allow you to convert concave meshes into convex ones.\nThis will create a new asset in a folder next to this scene, and will overwrite pre-existing ones, so make sure to use different names for different objects.", MessageType.None);
             if (GUILayout.Button("Generate Meshes"))
             {
                 m_target.GenerateMeshes();
