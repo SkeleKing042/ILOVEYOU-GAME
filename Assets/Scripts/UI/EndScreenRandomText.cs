55f99e05bbf263a5deb356168414aa4d2ddf2b0d
Jackson Lucas <s210201@students.aie.edu.au>
2024-12-03 11:18:33 +1030

    Added noise effect on game end.
    Text appears on top of noise - text is pulled from a random table.

---
+++ Assets/Scripts/UI/EndScreenRandomText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ILOVEYOU.UI
+{
+    public class EndScreenRandomText : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI m_victoryText;
+        [SerializeField] private string[] m_victoryStrings;
+        [SerializeField] private TextMeshProUGUI m_lossText;
+        [SerializeField] private string[] m_lossStrings;
+
+        public void ApplyRandomVictory()
+        {
+            if(m_victoryStrings.Length > 0)
+                m_victoryText.text = m_victoryStrings[Random.Range(0, m_victoryStrings.Length)];
+        }
+        public void ApplyRandomLoss()
+        {
+            if(m_lossStrings.Length > 0)
+                m_lossText.text = m_lossStrings[Random.Range(0, m_lossStrings.Length)];
+        }
+    }
+}
